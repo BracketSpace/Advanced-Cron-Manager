0c3776706321e97f81dbc25677dcb4eb530f585e
Kuba Mikita <jakub@underdev.it>
2017-06-07 22:05:24 +0200

    initial commit

---
+++ base.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Advanced Cron Manager
+ * Description: Plugin that allow to view, remove, edit and add WP Cron tasks.
+ * Version: 2.0
+ * Author: underDEV
+ * Author URI: https://underdev.it
+ * License: GPL2
+ * Text Domain: advanced-cron-manager
+ */
+
+
