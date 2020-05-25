43678e0d24056d2c0fadc6bbb62171a300eab530
Viktoriia Iasynetska <viktoriia.iasynetska@gmail.com>
2020-05-25 10:05:15 +0200

    set ascending default sorting for column next-execution #2wgk5f

--- views/parts/events/header-row.php
+++ views/parts/events/header-row.php
@@ -13,6 +13,6 @@
 		<div class="column event is-sortable" data-name="event"><?php esc_html_e( 'Event', 'advanced-cron-manager' ); ?></div>
 		<div class="column schedule is-sortable" data-name="schedule"><?php esc_html_e( 'Schedule', 'advanced-cron-manager' ); ?></div>
 		<div class="column arguments"><?php esc_html_e( 'Arguments', 'advanced-cron-manager' ); ?></div>
-		<div class="column next-execution is-sortable" data-name="next-execution"><?php esc_html_e( 'Next execution', 'advanced-cron-manager' ); ?></div>
+		<div class="column next-execution is-sortable asc" data-name="next-execution"><?php esc_html_e( 'Next execution', 'advanced-cron-manager' ); ?></div>
 	</div>
 </div>
