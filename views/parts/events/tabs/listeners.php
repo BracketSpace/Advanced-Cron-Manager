59e4e3f45e963686329690f4114164751c17a4e2
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-11-05 14:36:22 +0100

    feat: listeners tab feature

---
+++ views/parts/events/tabs/listeners.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Listeners tab
+ * View scope is the same as in the events/section view
+ *
+ * @package advanced-cron-manager
+ */
+
+?>
+
+<p> See the list of all the functions and methods hooked into this event.
+	<br>
+	<a href="https://bracketspace.com/downloads/advanced-cron-manager-pro/" target="_blank">Get Advanced Cron Manager PRO</a>
+</p>
