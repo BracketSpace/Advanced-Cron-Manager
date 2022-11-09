44d4dc5c9aa856c4499309c5b4e0d208089603bf
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-11-09 18:38:02 +0100

    fix: text in esc_html_e

--- views/forms/footer.php
+++ views/forms/footer.php
@@ -9,7 +9,7 @@
 
 <div class="submit-row">
 	<button type="submit" class="button button-primary send-form"><?php echo esc_attr( $this->get_var( 'cta' ) ); ?></button>
-	<a href="#" class="button button-secondary add-argument">Add argument</a>
+	<a href="#" class="button button-secondary add-argument"><?php esc_html_e( 'Add argument', 'advanced-cron-manager' ); ?></a>
 	<span class="spinner"></span>
 </div>
 
