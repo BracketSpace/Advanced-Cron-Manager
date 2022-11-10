89b8c63b9d862707b355e1de4b1adc2ca38caa13
Krystian Wojciechowski <wojciechowskik1010@gmail.com>
2022-11-10 14:04:30 +0100

    fix: styles of buttons

--- views/forms/footer.php
+++ views/forms/footer.php
@@ -8,9 +8,9 @@
 ?>
 
 <div class="submit-row">
-	<button type="submit" class="button button-primary send-form"><?php echo esc_attr( $this->get_var( 'cta' ) ); ?></button>
 	<a href="#" class="button button-secondary add-argument"><?php esc_html_e( 'Add argument', 'advanced-cron-manager' ); ?></a>
-	<span class="spinner"></span>
+	<button type="submit" class="button button-primary send-form"><?php echo esc_attr( $this->get_var( 'cta' ) ); ?></button>
 </div>
+<span class="spinner"></span>
 
 </form>
